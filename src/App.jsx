1b1850d0b50af12264436e8f37f21354842c0a48
ashishmp007 <ashishphadtare007@gmail.com>
2026-03-09 10:00:00 +0530

    favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,26 @@ import Search from "./pages/Search";
 import Favorites from "./pages/Favorites";
 import MovieDetails from "./pages/MovieDetails";
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
   return (
     <Router>
-      <Navbar />
+      <Navbar toggleTheme={toggleTheme} theme={theme} />
+
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
-        <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
