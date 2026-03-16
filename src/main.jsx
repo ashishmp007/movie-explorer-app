df00620555f447c003826d86b2742fb55ff24560
ashishmp007 <ashishphadtare007@gmail.com>
2026-03-16 14:50:02 +0530

    login needed to add favroite

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+  import { AuthProvider } from "./context/AuthContext"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="680468392827-6dbbjdvcfav77nbbn48pkfivc67vesj4.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="...">
+    <AuthProvider>
       <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>,
+    </AuthProvider>
+  </GoogleOAuthProvider>
+</React.StrictMode>
 );
